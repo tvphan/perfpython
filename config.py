f67a1ba46843f02d41e1689feb491f814579d570
MATTHIAS A. LEE <malee@us.ibm.com>
2015-06-04 16:43:09 -0400

    added empty config file

---
+++ config.py
@@ -0,0 +1,11 @@
+config={
+    
+# URL to the cloudant instance
+"dburl" : ("https://<dbURL>"),
+
+# Username & Password 
+"auth" : ('<username>','<password'),
+
+# Database to be used
+"dbname" : "testdb"
+}
