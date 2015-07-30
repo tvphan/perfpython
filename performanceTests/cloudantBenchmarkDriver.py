58ac36719fd25a305509324a91ad226cf7a00959
MATTHIAS A. LEE <malee@us.ibm.com>
2015-07-29 20:54:46 -0400

    getting started on abstracting the BenchmarkDriver in order to allow us to share common code between multiple different benchmarks, related to CloudDataServices/perf-cloudant-benchmark#21

---
+++ performanceTests/cloudantBenchmarkDriver.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+##################################################################
+# Licensed Materials - Property of IBM
+# (c) Copyright IBM Corporation 2015. All Rights Reserved.
+# 
+# Note to U.S. Government Users Restricted Rights:  Use,
+# duplication or disclosure restricted by GSA ADP Schedule 
+# Contract with IBM Corp.
+##################################################################
+'''
+Created on Jul 29, 2015
+
+@author: madmaze
+'''
+import abstractBenchmarkDriver
+
+class cloudantBenchmarkDriver(abstractBenchmarkDriver.abstractBenchmarkDriver):
+    '''
+    classdocs
+    '''
+    
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        super().__init__()
+        
+    def setUp(self):
+        super().setUp()
+        
+    def tearDown(self):
+        super().tearDown()
+        
+    def threadWorker(self):
+        super().threadWorker()
+        
+    
+        
